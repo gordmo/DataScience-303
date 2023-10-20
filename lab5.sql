14d0d8f4b7c0532b7aec69e1aa591444d9823268
Gordon Moore <gjmoore13@gmail.com>
2023-10-20 16:24:13 -0600

    add lab5

--- lab5.sql
+++ lab5.sql
@@ -2,7 +2,7 @@
 SELECT COUNT(*) 
 FROM album 
 JOIN album_genre ON album.id = album_genre.album_id 
-WHERE genre = 'Jazz';
+WHERE genre = 'jazz';
 
 -- 2. oldest album release year
 SELECT MIN(year) 
@@ -11,7 +11,7 @@ FROM album;
 -- 3. solo artist count
 SELECT COUNT(*) 
 FROM artist 
-WHERE type = 'solo';
+WHERE type = 'Person';
 
 -- 4. artist's w/ names longer than 5 chars
 SELECT COUNT(*) 
@@ -32,10 +32,14 @@ HAVING COUNT(track.number) > 20
 ORDER BY COUNT(track.number) DESC, album.title ASC;
 
 -- 7. avg years spent in group
--- Assuming a field end_year in group_member table to track membership duration
-SELECT AVG(end_year - EXTRACT(YEAR FROM joined_date)) 
-FROM group_member 
-WHERE end_year IS NOT NULL;
+SELECT AVG(
+    CASE 
+        WHEN end_year IS NOT NULL THEN end_year - begin_year
+        ELSE EXTRACT(YEAR FROM NOW()) - begin_year
+    END
+) AS average_years
+FROM artist_member_xref
+WHERE begin_year IS NOT NULL;
 
 -- 8. years w/ exactly 8 album releases
 SELECT year, COUNT(*) 
@@ -44,12 +48,13 @@ GROUP BY year
 HAVING COUNT(*) = 8;
 
 -- 9. max # of diff members in a group
+-- why does this not work?
 SELECT MAX(member_count) 
 FROM (
     SELECT COUNT(*) AS member_count 
     FROM group_member 
-    GROUP BY id
-) AS member_counts;
+    GROUP BY id) 
+AS member_counts;
 
 -- 10. artist w/ most recent album
 SELECT artist.name, MAX(album.year) 
@@ -60,28 +65,44 @@ ORDER BY MAX(album.year) DESC
 LIMIT 1;
 
 -- 11. groups christ thile has been in
-SELECT group_name 
+SELECT name 
 FROM group_member 
 WHERE name = 'Chris Thile';
 
 -- 12. members of foo fighters
--- Assuming a field end_year in group_member table to track membership duration
-SELECT name, COALESCE(end_year, EXTRACT(YEAR FROM NOW())) - EXTRACT(YEAR FROM joined_date) AS years 
-FROM group_member 
-WHERE group_name = 'Foo Fighters' 
-ORDER BY years DESC, name ASC;
+-- Im only getting massive attack here for some reason
+WITH FooFightersMembers AS (
+    SELECT member_id, begin_year, end_year 
+    FROM artist_member_xref 
+    JOIN artist ON artist.id = artist_member_xref.artist_id 
+    WHERE artist.name = 'Foo Fighters'
+)
+SELECT artist.name, 
+    CASE 
+        WHEN FooFightersMembers.end_year IS NOT NULL THEN FooFightersMembers.end_year - FooFightersMembers.begin_year
+        ELSE EXTRACT(YEAR FROM NOW()) - FooFightersMembers.begin_year
+    END AS years
+FROM FooFightersMembers
+JOIN artist ON FooFightersMembers.member_id = artist.id
+ORDER BY years DESC, artist.name ASC;
 
 -- 13. amy ray's 2020 album and producing label
-SELECT album.title, label.name 
-FROM album 
-JOIN artist ON album.artist_id = artist.id 
-JOIN label ON album.label_id = label.id 
-WHERE artist.name = 'Amy Ray' AND album.year = 2020;
+SELECT album.title, label.name
+FROM group_member
+JOIN artist_member_xref ON group_member.id = artist_member_xref.member_id
+JOIN artist ON artist.id = artist_member_xref.artist_id
+JOIN album ON album.artist_id = artist.id
+JOIN label ON album.label_id = label.id
+WHERE group_member.name = 'Amy Ray' AND album.year = 2020;
 
 -- 14. artists w/ more than 10 songs from same label
-SELECT artist.name 
-FROM artist 
-JOIN album ON artist.id = album.artist_id 
-GROUP BY artist.name, album.label_id 
-HAVING COUNT(album.id) > 10;
+SELECT artist.name
+FROM artist
+WHERE EXISTS (
+    SELECT 1
+    FROM album
+    WHERE artist.id = album.artist_id
+    GROUP BY album.label_id
+    HAVING COUNT(album.id) > 10
+);
 
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Gordon Moore
+I really struggled with the Amy Ray one, but only because it required so many joins.
+I liked learning more about subqueries, I think they're super powerful
+I spent about 4.5 hours on this assignment.
